4ca6f3efb48d6781b105f6110680e176ce99a1d8
Christian Kozalla <christian.kozalla@skriptfabrik.com>
2026-06-30 09:07:38 +0200

    chore: use correct module resolution for typescript v6 #1

--- packages/request-with-auth-bundle/src/utils/request.ts
+++ packages/request-with-auth-bundle/src/utils/request.ts
@@ -6,12 +6,9 @@ const _cache: { axiosInstance: AxiosInstance | null } = {
 
 export async function getAxios() {
 	if (!_cache.axiosInstance) {
-		// @ts-expect-error import is sufficient
 		// eslint-disable-next-line unicorn/no-await-expression-member
 		const axios = (await import('axios')).default;
-		// @ts-expect-error import is sufficient
 		const { Agent: AgentHttp } = await import('node:http');
-		// @ts-expect-error import is sufficient
 		const { Agent: AgentHttps } = await import('node:https');
 
 		const httpAgent = new AgentHttp();
